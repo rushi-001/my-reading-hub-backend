1ee49c6fdc785cc7df241dfa419c5534bc6a4797
RUSHI <133478781+rushi-001@users.noreply.github.com>
2026-03-19 11:12:13 +0530

    Updated code

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 import { Server } from "./server.js";
 
 const app = new Server();
+
+app.start();
--- netlify/functions/index.js
+++
@@ -1,6 +0,0 @@
-import serverless from "serverless-http";
-import { Server } from "../../server.js";
-
-const app = new Server().server;
-
-export const handler = serverless(app);
